fc1defe575d2bc63a43e1632018a6cf25b03f183
sergey <sergey@cimon.io>
2026-03-13 04:10:33 +0200

    commands: use dict to configure handlers

--- tests/cli/test_cli_contacts.py
+++ tests/cli/test_cli_contacts.py
@@ -66,4 +66,4 @@ def test_contacts_management(capsys, monkeypatch):
         assert run_cli(commands, capsys, monkeypatch) == expected
 
         mock_load.assert_called_once_with(default_factory=AddressBook)
-        assert_mock_called_n_times_with(mock_save, len(commands), (book,))
+        assert_mock_called_n_times_with(mock_save, len(commands) - 1, (book,))
